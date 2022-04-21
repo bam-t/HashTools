ea0efff95390fa182841efde93c2287f2346d5bd
BamTW <beamlaktamene@gmail.com>
2022-04-21 11:42:29 +0300

    added event listener to open file dialog when file browse button is pressed

--- HashTools/MainWindow.xaml.cs
+++ HashTools/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,46 @@ namespace HashTools
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filePath = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_browseFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lbl_hashResult.Content = string.Empty;
+                lbl_filePath.Content = string.Empty;
+                OpenFileDialog openFileDialog = new OpenFileDialog()
+                {
+                    CheckFileExists = true,
+                    Multiselect = false,
+                    ValidateNames = true,
+                    Title = "Select File to Verify Hash"
+                };
+                if (openFileDialog.ShowDialog() is true)
+                {
+                    filePath = openFileDialog.FileName;
+                    lbl_filePath.Content = filePath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"There was an error: {ex.Message}");
+            }
+        }
+
+        private void btn_GenerateHash_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_verifyHash_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
